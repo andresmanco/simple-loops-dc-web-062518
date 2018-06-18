7386a3dccda342fadb63da0ff44435cfa8127226
Andres Manco <andresmanco@gmail.com>
2018-06-18 21:48:14 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -6,7 +6,8 @@ def loop_iterator(number_of_times)
   loop do
     puts "Welcome to Flatiron School's Web Development Course!"
     n+=1
-    break if n == number_of_times
+    if n == number_of_times
+      break
     end
   end
 end
